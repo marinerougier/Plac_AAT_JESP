b64979ee7ef185e28cac2e28453d3e77177fd09c
Marine Rougier <m.rougier@hotmail.fr>
2024-06-26 16:33:51 +0200

    changes

--- experiment_vaast.js
+++ experiment_vaast.js
@@ -800,7 +800,7 @@ var vaast_training = {
     save_vaast_trial
   ],
   timeline_variables: vaast_stim_training,
-  repetitions: 1, // PUT 4 FOR 64 trials
+  repetitions: 4, // PUT 4 FOR 64 trials
   randomize_order: true,
   data: {
     phase: "training",
